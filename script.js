f3cf1afd8ea3d761fbd162a5f65bf5c3d3295030
LAPTOP-4F1S1RU2\fawen <jessica850501@gmail.com>
2017-08-30 11:12:56 +0800

    第二週作業

---
+++ script.js
@@ -0,0 +1,36 @@
+function max(a, b) {
+    //    console.log(a>b);
+    if (a > b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+
+function squares(a, b) {
+    return a * a + b * b;
+}
+
+
+function squares(a, b) {
+    return Math.pow(a, 2) + Math.pow(b, 2)
+}
+
+
+function leapYear(y) {
+    //    console.log(y%4===0);
+    //    console.log(y%100!=0);
+    //    console.log(y%400===0);
+    //    console.log(y % 100 != 0 || y % 400 === 0);
+
+    //    var yy = y % 100 != 0 || y % 400 === 0;
+    //    console.log(y % 4 === 0 && yy);
+    if (y % 4 === 0 && (y % 100 != 0 || y % 400 === 0)) {
+        return "yes"
+    }
+    else {
+        return "no"
+    }
+
+}
